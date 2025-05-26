6700c8f47aa0d2215f43886d858653200f33443a
fitzsanz <159743646+fitzsanz@users.noreply.github.com>
2025-05-26 18:04:55 +0800

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,8 +6,8 @@ function Blog() {
 
       <article>
         <h2>🌊 Day 1: Ricoro Falls, Barangay Ricoro, Gingoog City</h2>
-        <img src="public/falls1.png" alt="Ricoro Falls" />
-        <img src="public/falls4.png" alt="Ricoro Falls 2" />
+        <img src="/falls1.png" alt="Ricoro Falls" />
+        <img src="/falls4.png" alt="Ricoro Falls 2" />
         <p>
           “Every step forward is a step closer to nature’s secret.”
 
@@ -17,8 +17,8 @@ function Blog() {
 
       <article>
         <h2>🏕 Day 2: Camping by Manibay Falls, Sitio Manibay, Claveria</h2>
-        <img src="public/falls2.png" alt="Falls 2" />
-        <img src="public/falls5.png" alt="Falls 5" />
+        <img src="/falls2.png" alt="Falls 2" />
+        <img src="/falls5.png" alt="Falls 5" />
         <p>
           Our second day led us to Manibay Falls, nestled in Sitio Manibay, Claveria—a breathtaking destination that tested both our courage and determination. To get there, we had to cross a dangerous river, its current swift and unpredictable, challenging every step with uncertainty. But with teamwork and care, we made it across, and what greeted us was beyond imagination—a powerful cascade hidden behind wild vines and ancient rocks. The danger we faced made the moment even more surreal. Manibay Falls was more than a sight; it was a victory earned through bravery.
         </p>
@@ -26,8 +26,8 @@ function Blog() {
 
       <article>
         <h2>🌄 Day 3: Hidden Gem - Lantawon Falls, Sitio Lantawon, Claveria</h2>
-        <img src="public/falls6.png" alt="Lantawon Falls" />
-        <img src="public/falls3.png" alt="Lantawon Falls" />
+        <img src="/falls6.png" alt="Lantawon Falls" />
+        <img src="/falls3.png" alt="Lantawon Falls" />
         <p>
           Our final day brought us to the towering beauty of Lantawon Falls in Sitio Lantawon, Claveria. This leg of the adventure demanded strength as we climbed through steep, high terrain just to catch a glimpse of its hidden grandeur. Each incline pushed our limits, with every rock and tree root a step closer to the top. But reaching the falls felt like standing at nature’s throne—an isolated haven where water danced from the cliffs like silver threads. Surrounded by towering trees and mist-kissed air, Lantawon Falls was a climactic end to our journey, a waterfall we had to earn with effort and awe.
 
